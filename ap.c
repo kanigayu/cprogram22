4cff7264e9a411e07549962c19319bd69868c6d9
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-02-02 16:59:21 +0530

    ap.c
    ap

---
+++ ap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int a,d,n,sum,tn;
+printf("enter the num");
+scanf("%d",&n);
+printf("Enter the first num");
+scanf("%d",&a);
+printf("enter the diference");
+scanf("%d",&d);
+sum=n*(2*(a+(n-1)*d))/2;
+tn=a+(n-1)d;
+printf("%d",sum);
+printf("%d",tn);
+return 0;
+}
+
+
