da4949013decc3f94b4cca416b0bb73efaf43d62
kanigayu <31438605+kanigayu@users.noreply.github.com>
2018-02-05 16:02:44 +0530

    ap.c
    ap

--- ap.c
+++ ap.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
 int main()
 {
-int a,d,n,sum,tn;
+int a,d,n,i,sum,value;
 printf("enter the num");
 scanf("%d",&n);
 printf("Enter the first num");
 scanf("%d",&a);
 printf("enter the diference");
 scanf("%d",&d);
-sum=n*(2*(a+(n-1)*d))/2;
-tn=a+(n-1)d;
-printf("%d",sum);
-printf("%d",tn);
+value = a;
+printf("AP SERIES\n");
+for(i = 0; i<n; i++)
+{
+printf("%d ", value);
+sum += value;
+value = value + d;
+}
+printf(" %d\n",n,sum);
 return 0;
 }
-
 
